87dfff9a1c7043a8aa675428f621f3b80dec23ca
Priyank Saini <priyanksaini2010@gmail.com>
2018-08-14 15:05:39 +0530

    New Home Page Added

--- themes/cart/views/cart/home.php
+++ themes/cart/views/cart/home.php
@@ -1,109 +1,111 @@
 <?php $baseUrl = (Yii::app()->theme ? $baseUrl : Yii::app()->request->baseUrl . "/themes/cart"); ?>
 <div class="container">
-				<div class="row">
-				
-                    <div class="home-cta-wrapper">
-                    <div class="col-xs-12 col-md-6 col-lg-3 cta-home">
-                        <div class="cta-home-wrap">
-                        <a href="<?php echo Yii::app()->createUrl('cart/student'); ?>" class="cta-link1">
-                            <span class="cta-img"><img src="<?php echo $baseUrl; ?>/images/student.png">
-							</span>
-                            <span class="cta-title">Students</span>
-                        </a>
-                        <div class="cta-body">
-                            <div class="cta-body-row">
-                                <a href="student.html"><span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta1.1.png"></span></a>
-                                <span class="body-text">Become Industry Ready and Relevant</span>
-                            </div>
-                            <div class="cta-body-row">
-                                <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta1.2.png"></span>
-                                <span class="body-text">Be prepared for transition to corporate life</span>
-                            </div>
-                            <div class="cta-body-row">
-                                <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta1.3.png"></span>
-                                <span class="body-text">Build / develop the right skills for success</span>
-                            </div>
+    <div class="row">
+
+        <div class="home-cta-wrapper">
+            <h4 class="home_cta_title">End to End solutions to develop 'Ready' and 'Relevant' corporate executives</h4>
+            <h3 class="home_cta_sub_title">Begin your #MBAtrek Journy as...</h3>
+            <div class="col-xs-12 col-md-6 col-lg-3 cta-home">
+                <div class="cta-home-wrap">
+                    <a href="<?php echo Yii::app()->createUrl('cart/student');?>" class="cta-link1">
+                        <span class="cta-img"><img src="images/student.png">
+                        </span>
+                        <span class="cta-title">Students</span>
+                    </a>
+                    <div class="cta-body">
+                        <div class="cta-body-row">
+                            <a href="<?php echo Yii::app()->createUrl('cart/student');?>"><span class="body-icon"><img src="images/cta1.1.png"></span></a>
+                            <span class="body-text">Become Industry Ready and Relevant</span>
                         </div>
-                        <div class="cta-link2">
-                            <a href="<?php echo Yii::app()->createUrl('cart/student'); ?>">Know More >></a>
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta1.2.png"></span>
+                            <span class="body-text">Be prepared for transition to corporate life</span>
                         </div>
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta1.3.png"></span>
+                            <span class="body-text">Build / develop the right skills for success</span>
                         </div>
                     </div>
-                    <div class="col-xs-12 col-md-6 col-lg-3 cta-home">
-                            <div class="cta-home-wrap">
-                            <a href="<?php echo Yii::app()->createUrl('cart/profesionals'); ?>" class="cta-link1">
-                                <span class="cta-img"><img src="<?php echo $baseUrl; ?>/images/young.png"></span>
-                                <span class="cta-title">Young Professionals</span>
-                            </a>
-                            <div class="cta-body">
-                                <div class="cta-body-row">
-                                    <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta2.1.png"></span>
-                                    <span class="body-text">Enable you to “hit the ground RUNNING”</span>
-                                </div>
-                                <div class="cta-body-row">
-                                    <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta2.2.png"></span>
-                                    <span class="body-text">Manage and grow in the first few years at work</span>
-                                </div>
-                                <div class="cta-body-row">
-                                    <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta2.3.png"></span>
-                                    <span class="body-text">Build a successful career by engaging with industry people</span>
-                                </div>
-                            </div>
-                            <div class="cta-link2">
-                                <a href="<?php echo Yii::app()->createUrl('cart/profesionals'); ?>">Know More >></a>
-                            </div>
-                            </div>
-                    </div>
-                    <div class="col-xs-12 col-md-6 col-lg-3 cta-home">
-                            <div class="cta-home-wrap">
-                            <a href="#" class="cta-link1">
-                                <span class="cta-img"><img src="<?php echo $baseUrl; ?>/images/educational.png"></span>
-                                <span class="cta-title">Educational Institutes</span>
-                            </a>
-                            <div class="cta-body">
-                                <div class="cta-body-row">
-                                    <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta3.1.png"></span>
-                                    <span class="body-text">Help your students prepare for internships and placements</span>
-                                </div>
-                                <div class="cta-body-row">
-                                    <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta3.2.png"></span>
-                                    <span class="body-text">Partner with you to deliver a career building proposition </span>
-                                </div>
-                                <div class="cta-body-row">
-                                    <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta3.3.png"></span>
-                                    <span class="body-text">Help improve the ROI of your programs</span>
-                                </div>
-                            </div>
-                            <div class="cta-link2">
-                                <a href="#">Know More >></a>
-                            </div>
-                            </div>
-                    </div>
-                    <div class="col-xs-12 col-md-6 col-lg-3 cta-home">
-                            <div class="cta-home-wrap">
-                            <a href="<?php echo Yii::app()->createUrl('cart/institutes'); ?>" class="cta-link1">
-                                <span class="cta-img"><img src="<?php echo $baseUrl; ?>/images/company.png"></span>
-                                 <span class="cta-title">Companies</span>
-                            </a>
-                            <div class="cta-body">
-                                <div class="cta-body-row">
-                                    <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta4.1.png"></span>
-                                    <span class="body-text">On-board young talent to corporate way of life</span>
-                                </div>
-                                <div class="cta-body-row">
-                                    <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta4.2.png"></span>
-                                    <span class="body-text">Develop your young talent as problem solvers and critical thinkers</span>
-                                </div>
-                                <div class="cta-body-row">
-                                    <span class="body-icon"><img src="<?php echo $baseUrl; ?>/images/cta4.3.png"></span>
-                                    <span class="body-text">Build stability / focus in workforce to help manage hiring costs</span>
-                                </div>
-                            </div>
-                            <div class="cta-link2">
-                                <a href="<?php echo Yii::app()->createUrl('cart/institutes'); ?>">Know More >></a>
-                            </div>
-                        </div>
+                    <div class="cta-link2">
+                        <a href="<?php echo Yii::app()->createUrl('cart/student');?>">Know More >></a>
                     </div>
                 </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3 cta-home">
+                <div class="cta-home-wrap">
+                    <a href="<?php echo Yii::app()->createUrl('cart/profesionals'); ?>" class="cta-link1">
+                        <span class="cta-img"><img src="images/young.png"></span>
+                        <span class="cta-title">Young Professionals</span>
+                    </a>
+                    <div class="cta-body">
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta2.1.png"></span>
+                            <span class="body-text">Enable you to “hit the ground RUNNING”</span>
+                        </div>
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta2.2.png"></span>
+                            <span class="body-text">Manage and grow in the first few years at work</span>
+                        </div>
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta2.3.png"></span>
+                            <span class="body-text">Build a successful career by engaging with industry people</span>
+                        </div>
+                    </div>
+                    <div class="cta-link2">
+                        <a href="<?php echo Yii::app()->createUrl('cart/profesionals'); ?>">Know More >></a>
+                    </div>
                 </div>
-				</div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3 cta-home">
+                <div class="cta-home-wrap">
+                    <a href="<?php echo Yii::app()->createUrl('cart/institutes'); ?>" class="cta-link1">
+                        <span class="cta-img"><img src="images/educational.png"></span>
+                        <span class="cta-title">Educational Institutes</span>
+                    </a>
+                    <div class="cta-body">
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta3.1.png"></span>
+                            <span class="body-text">Help your students prepare for internships and placements</span>
+                        </div>
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta3.2.png"></span>
+                            <span class="body-text">Partner with you to deliver a career building proposition </span>
+                        </div>
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta3.3.png"></span>
+                            <span class="body-text">Help improve the ROI of your programs</span>
+                        </div>
+                    </div>
+                    <div class="cta-link2">
+                        <a href="<?php echo Yii::app()->createUrl('cart/institutes'); ?>">Know More >></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3 cta-home">
+                <div class="cta-home-wrap">
+                    <a href="<?php echo Yii::app()->createUrl('cart/pages',array("view"=>"company")); ?>" class="cta-link1">
+                        <span class="cta-img"><img src="images/company.png"></span>
+                        <span class="cta-title">Companies</span>
+                    </a>
+                    <div class="cta-body">
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta4.1.png"></span>
+                            <span class="body-text">On-board young talent to corporate way of life</span>
+                        </div>
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta4.2.png"></span>
+                            <span class="body-text">Develop your young talent as problem solvers and critical thinkers</span>
+                        </div>
+                        <div class="cta-body-row">
+                            <span class="body-icon"><img src="images/cta4.3.png"></span>
+                            <span class="body-text">Build stability / focus in workforce to help manage hiring costs</span>
+                        </div>
+                    </div>
+                    <div class="cta-link2">
+                        <a href="<?php echo Yii::app()->createUrl("cart/company");?>">Know More >></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
