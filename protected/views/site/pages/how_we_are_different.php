95375acd88cfca49af2f878f7c76724ef122707e
Priyank Saini <priyanksaini2010@gmail.com>
2018-09-30 18:39:45 +0530

    SEO URLs

--- protected/views/site/pages/how_we_are_different.php
+++ protected/views/site/pages/how_we_are_different.php
@@ -3,7 +3,7 @@
 <div class="bread_crum">
     <ul class="list-inline list-unstyled">
 	<li><a href="javascript:void(0);"><i class="fa fa-home" aria-hidden="true"></i></a> <i class="fa fa-angle-right" aria-hidden="true"></i></li>
-	<li><a href="<?php echo Yii::app()->createUrl('site/index'); ?>">Home</a> <i class="fa fa-angle-right" aria-hidden="true"></i></li>
+	<li><a href="<?php echo Yii::app()->createUrl('cart/index'); ?>">Home</a> <i class="fa fa-angle-right" aria-hidden="true"></i></li>
 	<li class="active"><a href="javascript:void(0);">How are we different</a></li>
     </ul>
 </div>
