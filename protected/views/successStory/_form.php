44afc8a4ee8d4a8bec73f7691ddeafaf815def2e
Priyank Saini <priyanksaini2010@gmail.com>
2018-10-14 20:24:32 +0530

    System Changes

---
+++ protected/views/successStory/_form.php
@@ -0,0 +1,35 @@
+<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+	'id'=>'success-story-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="help-block">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<?php
+        $ar = array(1=>"Placement Coordinators",2=>"students ",3=>"Young Professionals");
+        echo $form->dropDownList($model, 'type', $ar, array('class' => 'span3')); ?>
+
+	<?php
+        
+        $ar = array(1=>"#YouBeTheExecutive",2=>"How to Leverage 2 Year MBA Program",3=>"How to Maximize the Opportunity for Placements",4=>"Left Part",5=>"Right Part");
+         echo $form->dropDownList($model, 'sub_type', $ar, array('class' => 'span3')); ?>
+
+	<?php echo $form->textFieldRow($model,'college_or_company',array('class'=>'span5','maxlength'=>255)); ?>
+
+	<?php echo $form->textFieldRow($model,'author',array('class'=>'span5','maxlength'=>255)); ?>
+
+	<?php echo $form->textFieldRow($model,'course',array('class'=>'span5','maxlength'=>255)); ?>
+
+	<?php echo $form->textFieldRow($model,'video_url',array('class'=>'span5','maxlength'=>255)); ?>
+
+	<div class="form-actions">
+		<?php $this->widget('bootstrap.widgets.TbButton', array(
+			'buttonType'=>'submit',
+			'type'=>'primary',
+			'label'=>$model->isNewRecord ? 'Create' : 'Save',
+		)); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
---
+++ protected/views/successStory/index.php
@@ -0,0 +1,17 @@
+<?php
+$this->breadcrumbs=array(
+	'Success Stories',
+);
+
+$this->menu=array(
+	array('label'=>'Create SuccessStory','url'=>array('create')),
+	array('label'=>'Manage SuccessStory','url'=>array('admin')),
+);
+?>
+
+<h1>Success Stories</h1>
+
+<?php $this->widget('bootstrap.widgets.TbListView',array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_view',
+)); ?>
