f3a33c3db011e1486b0d6ab1edd72379b764431f
Priyank Saini <priyanksaini2010@gmail.com>
2018-10-14 20:29:31 +0530

    System Changes

---
+++ themes/cart/views/cart/story.php
@@ -0,0 +1,82 @@
+<div class="container">
+    <div class="row">
+        <div class="sucess_story_wrap">
+            <h3>What students say about MBAtrek…</h3>
+            <div class="col-md-6 the_Executive">
+                <div class="sucess_heading">
+                    <h3>#YouBeTheExecutive</h3>
+                    <h2>Internship Series…</h2>
+                </div>
+                <div class="sucess_Repeat">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="video_container">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/THkYLBnHDfY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div class="col-md-7">
+                            <div class="sucess_info">
+                                <label>Ayushi Gupta MBA(2017-19) Amity Business School</label>
+                                <Span>Ayushi</Span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="sucess_Repeat">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="video_container">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/THkYLBnHDfY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div class="col-md-7">
+                            <div class="sucess_info">
+                                <label>Ayushi Gupta MBA(2017-19) Amity Business School</label>
+                                <Span>Ayushi</Span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="sucess_heading">
+                    <h3>#YouBeTheExecutive</h3>
+                    <h2>Internship Series…</h2>
+                </div>
+                <div class="sucess_Repeat">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="video_container">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/THkYLBnHDfY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div class="col-md-7">
+                            <div class="sucess_info">
+                                <label>Ayushi Gupta MBA(2017-19) Amity Business School</label>
+                                <Span>Ayushi</Span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="sucess_Repeat">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="video_container">
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/THkYLBnHDfY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div class="col-md-7">
+                            <div class="sucess_info">
+                                <label>Ayushi Gupta MBA(2017-19) Amity Business School</label>
+                                <Span>Ayushi</Span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="want_to_get">
+                <h3>Want to get these awesome career impacting packages <a href="javascript:void(0);">Click Here</a></h3>
+            </div>
+        </div>
+    </div>
+</div>
