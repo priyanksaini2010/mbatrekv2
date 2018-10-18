f22fc511866e3b7043bae8019ce065cc45b03a9b
Priyank Saini <priyanksaini2010@gmail.com>
2018-10-18 10:12:54 +0530

    Issue#85 : Resolved

--- protected/views/contact/contact.php
+++ protected/views/contact/contact.php
@@ -3,15 +3,15 @@
         <div class="maine_contact_heading">
             <h2>Let&rsquo;s Connect</h2>
             <h3>Looking forward to transforming you</h3>
-             <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
-                        'id'=>'contact-form',
-                        'enableAjaxValidation'=>false,
-                       'action' => Yii::app()->createUrl('contact/create'),
-                        'htmlOptions'=>array(
-                            'class'=>'form-horizontal',
-                            
-                            
-                    ))); ?> 
+            <?php
+            $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+                'id' => 'contact-form',
+                'enableAjaxValidation' => false,
+                'action' => Yii::app()->createUrl('contact/create'),
+                'htmlOptions' => array(
+                    'class' => 'form-horizontal',
+            )));
+            ?> 
             <div class="row">
                 <div class="col-md-6">
                     <div class="row">
@@ -19,41 +19,57 @@
                             <div class="contact_info_div margin-0">
                                 <div class="phAnimate">
                                     <label for="lastname">First Name <em>*</em></label> 
-                                    <?php if($model->first_name != ''){ $data =  array('for' => "first_name",'class' => 'active');}else{$data =  array('for' => "first_name");};?>
-                                    <?php echo $form->labelEx($model, 'first_name', $data); ?>
-                                    <?php echo $form->textField($model, 'first_name', array('class' => "input_field")); ?>
+                                    <?php if ($model->first_name != '') {
+                                        $data = array('for' => "first_name", 'class' => 'active');
+                                    } else {
+                                        $data = array('for' => "first_name");
+                                    }; ?>
+<?php echo $form->labelEx($model, 'first_name', $data); ?>
+<?php echo $form->textField($model, 'first_name', array('class' => "input_field")); ?>
                                 </div>
                             </div>
-                            
+
                         </div>
                         <div class="col-md-6">
                             <div class="contact_info_div margin-0">
                                 <div class="phAnimate">
                                     <!--<label for="lastname">Last Name <em>*</em></label>--> 
-                                    <?php if($model->last_name != ''){ $data =  array('for' => "last_name",'class' => 'active');}else{$data =  array('for' => "last_name");};?>
-                                    <?php echo $form->labelEx($model, 'last_name', $data); ?>
+<?php if ($model->last_name != '') {
+    $data = array('for' => "last_name", 'class' => 'active');
+} else {
+    $data = array('for' => "last_name");
+}; ?>
+<?php echo $form->labelEx($model, 'last_name', $data); ?>
                                     <?php echo $form->textField($model, 'last_name', array('class' => "input_field")); ?>
                                 </div>
                             </div>
-                            
+
                         </div>
                         <div class="col-md-6">
                             <div class="contact_info_div">
                                 <div class="phAnimate">
                                     <!--<label for="lastname">Email <em>*</em></label>--> 
-                                    <?php if($model->email != ''){ $data =  array('for' => "email",'class' => 'active');}else{$data =  array('for' => "email");};?>
-                                    <?php echo $form->labelEx($model, 'email', $data); ?>
-                                    <?php echo $form->textField($model, 'email', array('class' => "input_field")); ?>
+<?php if ($model->email != '') {
+    $data = array('for' => "email", 'class' => 'active');
+} else {
+    $data = array('for' => "email");
+}; ?>
+<?php echo $form->labelEx($model, 'email', $data); ?>
+<?php echo $form->textField($model, 'email', array('class' => "input_field")); ?>
                                 </div>
                             </div>
-                            
+
                         </div>
                         <div class="col-md-6">
                             <div class="contact_info_div">
                                 <div class="phAnimate">
-                                    <?php if($model->mobile_no != ''){ $data =  array('for' => "mobile_no",'class' => 'active');}else{$data =  array('for' => "mobile_no");};?>
-                                    <?php echo $form->labelEx($model, 'mobile_no', $data); ?>
-                                    <?php echo $form->textField($model, 'mobile_no', array('class' => "input_field")); ?>
+<?php if ($model->mobile_no != '') {
+    $data = array('for' => "mobile_no", 'class' => 'active');
+} else {
+    $data = array('for' => "mobile_no");
+}; ?>
+<?php echo $form->labelEx($model, 'mobile_no', $data); ?>
+<?php echo $form->textField($model, 'mobile_no', array('class' => "input_field")); ?>
                                 </div>
                             </div>
                         </div>
@@ -61,16 +77,20 @@
                             <div class="are_yu"><label>Are You?</label>
                                 <ul>
                                     <li>
-                                        <div class="check_box"><input id="checkboxG1" class="css-checkbox" type="checkbox" name="Contact[are_you]" value="Company Rep" /> <label class="css-label" for="checkboxG1">Company Rep</label></div>
+                                        <input type="radio"  name="Contact[type]" value="1"  id="radio2" class="css-checkbox" checked="checked"/>
+                                        <label for="radio2" class="css-label radGroup1">Company Rep</label>                                                    
                                     </li>
                                     <li>
-                                        <div class="check_box"><input id="checkboxG2" class="css-checkbox" type="checkbox" name="Contact[are_you]" value="Institute Rep" /> <label class="css-label" for="checkboxG2">Institute Rep</label></div>
+                                        <input type="radio" name="Contact[type]" value="2"  id="radio3" class="css-checkbox" />
+                                        <label for="radio3" class="css-label radGroup1">Institute Rep</label>
                                     </li>
                                     <li>
-                                        <div class="check_box"><input id="checkboxG3" class="css-checkbox" type="checkbox" name="Contact[are_you]" value="College Student"  /> <label class="css-label" for="checkboxG3">College Student</label></div>
+                                        <input type="radio"  name="Contact[type]" value="3"  id="radio31" class="css-checkbox" />
+                                        <label for="radio31" class="css-label radGroup1">College Student</label>
                                     </li>
                                     <li>
-                                        <div class="check_box"><input id="checkboxG4" class="css-checkbox" type="checkbox" name="Contact[are_you]" value="Young professional"  /> <label class="css-label" for="checkboxG4">Young professional</label></div>
+                                        <input type="radio"  name="Contact[type]" value="4"  id="radio32" class="css-checkbox" />
+                                        <label for="radio32" class="css-label radGroup1">Young professional</label>
                                     </li>
                                 </ul>
                             </div>
@@ -78,27 +98,39 @@
                         <div class="col-md-12">
                             <div class="contact_info_div">
                                 <div class="phAnimate">
-                                    <?php if($model->name_of_company_institute != ''){ $data =  array('for' => "name_of_company_institute",'class' => 'active');}else{$data =  array('for' => "name_of_company_institute");};?>
-                                    <?php echo $form->labelEx($model, 'name_of_company_institute', $data); ?>
-                                    <?php echo $form->textField($model, 'name_of_company_institute', array('class' => "input_field")); ?>
+<?php if ($model->name_of_company_institute != '') {
+    $data = array('for' => "name_of_company_institute", 'class' => 'active');
+} else {
+    $data = array('for' => "name_of_company_institute");
+}; ?>
+<?php echo $form->labelEx($model, 'name_of_company_institute', $data); ?>
+<?php echo $form->textField($model, 'name_of_company_institute', array('class' => "input_field")); ?>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="contact_info_div">
                                 <div class="phAnimate"> 
-                                    <?php if($model->subject != ''){ $data =  array('for' => "subject",'class' => 'active');}else{$data =  array('for' => "subject");};?>
-                                    <?php echo $form->labelEx($model, 'subject', $data); ?>
-                                    <?php echo $form->textField($model, 'subject', array('class' => "input_field")); ?>
+<?php if ($model->subject != '') {
+    $data = array('for' => "subject", 'class' => 'active');
+} else {
+    $data = array('for' => "subject");
+}; ?>
+<?php echo $form->labelEx($model, 'subject', $data); ?>
+<?php echo $form->textField($model, 'subject', array('class' => "input_field")); ?>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="contact_info_div">
                                 <div class="phAnimate">
-                                    <?php if($model->your_message != ''){ $data =  array('for' => "your_message",'class' => 'active');}else{$data =  array('for' => "your_message");};?>
-                                    <?php echo $form->labelEx($model, 'your_message', $data); ?>
-                                    <?php echo $form->textArea($model, 'your_message', array('class' => "input_field")); ?>
+<?php if ($model->your_message != '') {
+    $data = array('for' => "your_message", 'class' => 'active');
+} else {
+    $data = array('for' => "your_message");
+}; ?>
+<?php echo $form->labelEx($model, 'your_message', $data); ?>
+<?php echo $form->textArea($model, 'your_message', array('class' => "input_field")); ?>
                                 </div>
                             </div>
                         </div>
@@ -115,11 +147,11 @@
                     </div>
                 </div>
             </div>
-             
+
             <div class="capcha_div"><img src="images/capcha.png" alt="" />
                 <div class="submit_form_btn" ><button type="submit">Submit</button></div>
             </div>
-            <?php $this->endWidget(); ?>
+<?php $this->endWidget(); ?>
         </div>
     </div>
 </div><style>
