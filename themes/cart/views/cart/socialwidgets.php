e872b60f982bf8eda79d088c67460c8c3e41b99e
Priyank Saini <priyanksaini2010@gmail.com>
2018-10-20 12:50:41 +0530

    Issue#106 : Resolved

--- themes/cart/views/cart/socialwidgets.php
+++ themes/cart/views/cart/socialwidgets.php
@@ -2,15 +2,14 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-                <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-small-header="true" data-adapt-container-width="false" data-hide-cover="true" data-show-facepile="true">
-                <blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore">
-                    <a href="https://www.facebook.com/facebook">Facebook</a>
-                    </blockquote>
-                </div>
+                <div class="fb-page" data-href="https://www.facebook.com/MBAtrekIndia" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false">
+                    <blockquote cite="https://www.facebook.com/MBAtrekIndia" class="fb-xfbml-parse-ignore">
+                        <a href="https://www.facebook.com/MBAtrekIndia">MBAtrek Pvt Ltd.</a>
+                    </blockquote></div>
             </div>
             <div class="col-md-4">
                 <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
-                <script type="IN/CompanyProfile" data-id="1337" data-format="inline" data-related="false"></script>
+                <script type="IN/CompanyProfile" data-id="7955944" data-format="inline" data-related="false"></script>
             </div>
         </div>
     </div>
