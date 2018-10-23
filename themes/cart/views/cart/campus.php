193e1b5dd5c16215b05fe6f97bc734604c72fddc
Priyank Saini <priyanksaini2010@gmail.com>
2018-10-23 22:37:28 +0530

    Issue#106 : Added Instagram Feeds

---
+++ themes/cart/views/cart/campus.php
@@ -0,0 +1,121 @@
+<?php $this->setPageTitle('Campus Ambassador - Registeration'); ?>
+<div class="ambastor_container regsiter_emastor">
+    <section class="amastor_wrap">
+        <div class="container">
+            <div class="compas_heading">
+                <h3>Campus Ambassador Program</h3>
+                <h4>All the Best! Game On! </h4>
+            </div>
+        </div>
+    </section> 
+    <section class="earn_point">
+        <div class="container">
+            <div class="compas_heading">
+                <h4>How Do We Know You are Right for Us</h4>
+            </div>
+            <div class="earn_container">
+                <ul>
+                    <li>
+                        <img src="images/wow_icon.png"/>
+                        <span>Wow us with your answers</span>
+                    </li>
+                    <li>
+                        <img src="images/send_video_icon.png"/>
+                        <span>Send a video showing your awesomeness</span>
+                    </li>
+                    <li>
+                        <img src="images/final_interview_icon.png"/>
+                        <span>Final interview over skype</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>  
+    <section class="register_Form">
+        <div class="compas_heading">
+            <h4>Here’s Your Chance to Amaze us! </h4>
+        </div>
+        <div class="container">
+            <div class="col-md-6">
+                <div class="form_amster_field">
+                    <label>First Name <Em>*</Em></label>
+                    <input type="text"/>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form_amster_field">
+                    <label>Last Name <Em>*</Em></label>
+                    <input type="text"/>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form_amster_field">
+                    <label>Mobile Number <Em>*</Em></label>
+                    <input type="text"/>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form_amster_field">
+                    <label>E-mail ID <Em>*</Em></label>
+                    <input type="text"/>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form_amster_field">
+                    <label>E-mail ID <Em>*</Em></label>
+                    <select>
+                        <option>abc</option>
+                        <option>abc</option>
+                        <option>abc</option>
+                        <option>abc</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form_amster_field">
+                    <label>Course <Em>*</Em></label>
+                    <select>
+                        <option>abc</option>
+                        <option>abc</option>
+                        <option>abc</option>
+                        <option>abc</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form_amster_field">
+                    <label>Year of Graduation <Em>*</Em></label>
+                    <select>
+                        <option>abc</option>
+                        <option>abc</option>
+                        <option>abc</option>
+                        <option>abc</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form_amster_field">
+                    <label>Why do you want to be a MBAtrek Campus Ambassador? <Em>*</Em></label>
+                    <textarea></textarea>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form_amster_field">
+                    <label>Suggest two super creative ideas to share the importance of career development  in your college <Em>*</Em></label>
+                    <textarea></textarea>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form_amster_field">
+                    <label>Any additional information you would like to provide us?  </label>
+                    <textarea></textarea>
+                </div>
+            </div>
+            <div class="col-md-12 text-center">
+                <div class="form_amster_field">
+                    <input class="application_submit" type="submit" name="Submit Application" value="Submit Application"/>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
