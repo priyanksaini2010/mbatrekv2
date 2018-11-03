3a3852d9b7ec3aabc895e5c42de92413216e50a8
Priyank Saini <priyanksaini2010@gmail.com>
2018-11-03 20:46:47 +0530

    Issue#127 : Added HTML in POP Up

--- protected/views/eventGallery/admin.php
+++ protected/views/eventGallery/admin.php
@@ -16,10 +16,45 @@ $this->menu=array(
 
 
 
-<?php $this->widget('bootstrap.widgets.TbGridView',array(
+<?php
+$str_js = "
+		var fixHelper = function(e, ui) {
+			ui.children().each(function() {
+				$(this).width($(this).width());
+			});
+			return ui;
+		};
+				
+		$('#event-gallery-grid table.items tbody').sortable({
+			forcePlaceholderSize: true,
+			forceHelperSize: true,
+			items: 'tr',
+			update : function () {
+                        $('.ui-sortable-handle').removeClass('ui-sortable-handle')
+				serial = $('#event-gallery-grid table.items tbody').sortable('serialize', {key: 'items[]', attribute: 'class'});
+				$.ajax({
+					'url': '" . $this->createUrl('eventGallery/sort') . "',
+					'type': 'post',
+					'data': serial,
+					'success': function(data){
+					},
+					'error': function(request, status, error){
+						alert('We are unable to set the sort order at this time.  Please try again in a few minutes.');
+					}
+				});
+                                $('#event-gallery-grid table.items tr').addClass('ui-sortable-handle');
+			},
+			helper: fixHelper
+		}).disableSelection();
+	";
+Yii::app()->clientScript->registerScript('sortable-project', $str_js);
+
+
+$this->widget('bootstrap.widgets.TbGridView',array(
 	'id'=>'event-gallery-grid',
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
+     'rowCssClassExpression'=>'"items[]_{$data->id}"',
 	'columns'=>array(
 		'id',
 		'name',
