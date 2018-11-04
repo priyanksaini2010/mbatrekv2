a555d2e88de8a789bc0baedb77537a8298cb0acc
Priyank Saini <priyanksaini2010@gmail.com>
2018-11-04 17:12:11 +0530

    Issue#127 : Added HTML in POP Up

--- protected/views/videos/admin.php
+++ protected/views/videos/admin.php
@@ -1,61 +1,38 @@
 <?php
 $this->breadcrumbs=array(
-	'Videoses'=>array('index'),
+	'Videos'=>array('admin'),
 	'Manage',
 );
 
 $this->menu=array(
-	array('label'=>'List Videos','url'=>array('index')),
-	array('label'=>'Create Videos','url'=>array('create')),
+	array('label'=>'Manage Videos','url'=>array('videos/admin')),
+	array('label'=>'Create Videos','url'=>array('videos/create')),
 );
 
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$.fn.yiiGridView.update('videos-grid', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
 ?>
 
-<h1>Manage Videoses</h1>
+<h1>Manage Videos</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
-
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button btn')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
 
 <?php $this->widget('bootstrap.widgets.TbGridView',array(
 	'id'=>'videos-grid',
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
-		'id',
-		'blog_category_id',
-		'type',
+//		'id',
+//		'type',
 		'title',
 		'content',
-		'author',
-		/*
-		'background_image',
-		'banner_image',
+//		'youtube_video_link',
+                array( 'name'=>'youtube_video_link', 'type'=>'raw' ),
+
 		'date_created',
+		/*
 		'date_updated',
 		*/
 		array(
 			'class'=>'bootstrap.widgets.TbButtonColumn',
+                        'template' => '{update}{delete}',
 		),
 	),
 )); ?>
