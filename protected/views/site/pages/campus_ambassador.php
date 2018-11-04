90e28a33ba3956ae58f760175e1efc1f73fff173
Priyank Saini <priyanksaini2010@gmail.com>
2018-11-04 16:27:01 +0530

    Issue#127 : Added HTML in POP Up

--- protected/views/site/pages/campus_ambassador.php
+++ protected/views/site/pages/campus_ambassador.php
@@ -151,6 +151,7 @@
     <section class="faq_amastor">
        <div class="faq_new_container">
     <div class="container">
+        
         <h3>We at MBAtrek are here to help with all your career development needs Feel Free to Explore.</h3>
         <h4>Expand the following drop-downs for answers to frequently asked questions.</h4>
         <div class="faq_Container">
