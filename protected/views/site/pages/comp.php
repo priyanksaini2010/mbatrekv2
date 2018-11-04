902762a75c0bb55d797a43f73b0b42e04b4fd6e7
Priyank Saini <priyanksaini2010@gmail.com>
2018-11-04 19:19:41 +0530

    Issue#127 : Added HTML in POP Up

--- protected/views/site/pages/comp.php
+++ protected/views/site/pages/comp.php
@@ -7,6 +7,288 @@
 	<li class="active"><a href="javascript:void(0);">Company</a></li>
     </ul>
 </div>
-<?php $data  = ContentJson::model()->findByAttributes(array("page"=>"company_helping"));
-    echo $data->data;
-?>
+ <div class="page-wrapper">
+            <div class="our_company_page">
+                <section class="helping_section">
+                    <div class="container">
+                        <h3>Helping You Unlock the Potential of New Hires and Young Managers</h3>
+                        <p>
+                            As a recruiter or a HR lead or a hiring manger , do you feel that your new hires and younger professionals are struggling to cope with the corporate life and your company's culture.
+                        </p>
+                        <p>
+                            In fact, you will even notice that their idea generation and execution capabilities are also limited. Why do you believe this happens?
+                        </p>
+                        <p>
+                            This arises from lack of preparedness of the young graduate / early stage professionals with respect to being a 'Ready' and 'Relevant' professional. 
+                        </p>
+                        <div class="ready_process">
+                            <div class="student-journey">
+                                <div class="container">
+                                    <div class="student-journey-inner">
+                                    <div class="student-ready col-xs-12 col-md-6 col-lg-6">
+                                    <div class="journey-1">
+                                    <ul>
+                                    <li class="main-box"><span class="journey-text">Student</span></li>
+                                    <li class="box1"><span class="journey-text">Inner Realization</span></li>
+                                    <li class="box2"><span class="journey-text">Motivation</span></li>
+                                    </ul>
+                                    <div class="journey-title-bottom">Ready</div>
+                                   
+                                    </div>
+                                    </div>
+                                    <div class="student-relevant col-xs-12 col-md-6 col-lg-6">
+                                    <div class="journey-2">
+                                    <ul>
+                                    <li class="box1"><span class="journey-text">Corporate Skill Development</span></li>
+                                    <li class="box2"><span class="journey-text">Preparation &amp; Practise</span></li>
+                                    <li class="main-box"><span class="journey-text">Executive</span></li>
+                                    </ul>
+                                    <div class="journey-title-bottom">Relevant</div>
+                                  
+                                    </div>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+               <section class="helping_ready">
+                    <div class="container">
+                        <div class="student-ready col-xs-12 col-md-6 col-lg-6">
+                                <div class="journey-1">
+                                    <h2>Ready</h2>
+                                <div class="journey1-list">
+                                <ul>
+                                <li><span class="list-icon"><img src="images/contribute_icon2.png" alt=""></span> <span class="list-text"> <span class="list-text-title">Immediate Contribution:</span> Start delivering from Day 1 in their corporate role </span></li>
+                                <li><span class="list-icon"><img src="images/corporate_icon.png" alt=""></span> <span class="list-text"> <span class="list-text-title">Corporatiz-ed:</span> Understand today's corporate world and its demands </span></li>
+                                <li><span class="list-icon"><img src="images/industry_icon.png" alt=""></span> <span class="list-text"> <span class="list-text-title">Industry Awareness:</span> Ability to develop point of view on industry trends </span></li>
+                                </ul>
+                                </div>
+                                </div>
+                        </div>
+                        <div class="student-relevant col-xs-12 col-md-6 col-lg-6">
+                                <div class="journey-2">
+                                    <h2>Relevant</h2>
+                                <div class="journey2-list">
+                                <ul>
+                                <li><span class="list-icon"><img src="images/mix2.png" alt=""></span> <span class="list-text"> <span class="list-text-title">Right Mix:</span> Build analytical, logical and structured thinking skills </span></li>
+                                <li><span class="list-icon"><img src=" images/long_term.png" alt=""></span> <span class="list-text"> <span class="list-text-title">Long Term Career:</span> Develop a mindset not jumping between short term jobs </span></li>
+                                <li><span class="list-icon"><img src="images/collborate.png" alt=""></span> <span class="list-text"> <span class="list-text-title">Collaborate and Lead:</span> Demonstrate capability to work and lead teams </span></li>
+                                </ul>
+                                </div>
+                                </div>
+                        </div>
+                     </div>
+                </section>
+                <section class="bulid_your_next">
+                    <div class="container">
+                        <div class="arrow-icon"><a class="scroll_to" href="#followed_by"><img src="images/blue-arrow-down.jpg" alt=""></a></div> 
+                        <div class="student-top">
+                            <div class="container">
+                            <div class="student-internship col-xs-12 col-md-6 col-lg-6">
+                            <div class="icon-holder"><a id="tab1" href="javascript:void(0);"> <span class="icon"> <img src="images/company/dedicated_solution.png" alt=""> <span class="icon-title">Internship</span> </span> <span class="icon-text"> Need help in preparing for internship? <br><span style="text-decoration: underline;">Click Here</span></span> </a></div>
+                            </div>
+                            <div class="student-placement col-xs-12 col-md-6 col-lg-6">
+                            <div class="icon-holder"><a id="tab2" href="javascript:void(0);"> <span class="icon"> <img src="images/company/customize_solution.png" alt=""> <span class="icon-title">Final Placements</span> </span> <span class="icon-text"> Need help preparing for final placements? <span style="text-decoration: underline;display: block;">Click Here</span> </span> </a></div>
+                            </div>
+                            </div>
+                            <div class="arrow-icon"><a class="scroll_to" href="#followed_by"><img src="images/blue-arrow-down.jpg" alt=""></a></div> 
+                        </div>
+                    </div>
+                </section>
+                <section class="manager_div">
+                    <div class="container">
+                        <div class="manger_heading">
+                            <h2>#CAREERtrek</h2>
+                            <h3>Build Your Next Set of Empowered Managers </h3>
+                        </div>
+                        <div class="manger_categories">
+                            <ul>
+                                <li>
+                                    <img src="images/company/your_best_icon.png"/>
+                                    <span>Identify Your Best Managerial Talent</span>
+                                </li>
+                                <li>
+                                    <img src="images/company/assess_ther.png"/>
+                                    <span>Assess their Managerial Potential </span>
+                                </li>
+                                <li>
+                                    <img src="images/company/upp_skill.png"/>
+                                    <span>Up-Skill and Transition them to Decision Makers  </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+                <section class="empower_manager">
+                    <section class="manager_div">
+                            <div class="container">
+                                <div class="manger_heading">
+                                    <h2>CAREERtrek enables empowered managers to focus on </h2>
+                                </div>
+                                <div class="manger_categories">
+                                    <ul>
+                                        <li>
+                                            <img src="images/company/developing_people.png"/>
+                                            <span>Developing <br>People </span>
+                                        </li>
+                                        <li>
+                                            <img src="images/company/optamize_resources.png"/>
+                                            <span> Optimizing <br>Resources  </span>
+                                        </li>
+                                        <li>
+                                            <img src="images/company/business_process.png"/>
+                                            <span> Strengthening Business Processes </span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </section>
+                </section>
+               <section class="key_Area_cover">
+                    <div class="manager_div">
+                        <div class="container">
+                            <div class="manger_heading">
+                                <h2>Key Areas Covered</h2>
+                            </div>
+                            <div class="manger_categories">
+                                <ul>
+                                    <li>
+                                        <img src="images/company/role_icon.png"/>
+                                        <span>Roles, Ownership, Accountability, Resilience  </span>
+                                    </li>
+                                    <li>
+                                        <img src="images/company/imotional_icon.png"/>
+                                        <span>Emotional Intelligence </span>
+                                    </li>
+                                    <li>
+                                        <img src="images/company/role_icon.png"/>
+                                        <span> Roles, Responsibility, Objectives  </span>
+                                    </li>
+                                    <li>
+                                        <img src="images/company/effective_Team.png"/>
+                                        <span> Effective Team Member </span>
+                                    </li>
+                                    <li>
+                                        <img src="images/company/effective_feedback.png"/>
+                                        <span> Delivering and Receiving Effective Feedback </span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="arrow-icon"><a class="scroll_to" href="#followed_by"><img src="images/company/company_plus.png" alt=""></a></div> 
+                    <div class="support_managment">
+                        <div class="container">
+                            <div class="row">
+                                <div class="support_aligment">
+                                    <div class="support_block">
+                                        <h2>Managerial Potential On-line Assessment </h2>
+                                        <ul>
+                                            <li>Organisational Acumen</li>
+                                            <li>Collaboration</li>
+                                            <li>Leadership Skills</li>
+                                            <li>Interpersonal Skills</li>
+                                            <li>Managing Self</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="support_aligment pull-right">
+                                    <div class="support_block">
+                                        <h2>Support to Top Management   </h2>
+                                        <ul>
+                                            <li>Summary Action Report</li>
+                                            <li>De-brief with the </li>
+                                            <li>Top Management</li>
+                                            <li>Follow – up Exercises to </li>
+                                            <li>reinforce development</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="how_we_Deliver">
+                    <section class="manager_div">
+                        <div class="container">
+                            <div class="manger_heading">
+                                <h2>How Do We Deliver  </h2>
+                            </div>
+                            <div class="manger_categories">
+                                <ul>
+                                    <li>
+                                        <img src="images/company/at_your_office.png"/>
+                                        <span>At Your Office </span>
+                                    </li>
+                                    <li>
+                                        <img src="images/company/1.png"/>
+                                        <span>Days</span>
+                                    </li>
+                                    <li>
+                                        <img src="images/company/14.png"/>
+                                        <span>Hours Classroom </span>
+                                    </li>
+                                    <li>
+                                        <img src="images/company/ulimited_mail.png"/>
+                                        <span>Unlimited email support </span>
+                                    </li>
+                                    <li>
+                                        <img src="images/company/additional_Reading.png"/>
+                                        <span>Additional Reading Materials </span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </section> 
+                </section>
+                <section class="owner_proile">
+                    <div class="container">
+                        <div class="manger_heading">
+                            <h2>Looking for Customized Employee Career Development Solutions</h2>
+                            <h4>Connect with Our Leadership Team</h4>
+                        </div>
+                        <div id="profile_section" class="leader_shipt_div">
+                                <div class="row">
+                                <div class="col-md-6">
+                                <div class="team_container">
+                                <div class="team_photo"><img src="images/alok.jpg" alt=""></div>
+                                <div class="team_description">
+                                <h3>Alok Srivastava</h3>
+                                <span> Co-Founder, MBAtrek Pvt. Ltd.</span> <span>PGDBM (1975-77), XLRI</span> <span>Global Management Program, University of Michigan&nbsp;</span></div>
+                                <div class="contact_Details_team"><label>Mobile: +91-9810007333</label> <label>alok.srivastava@mbatrek.com</label></div>
+                                <div class="company_work">
+                                <h3>Companies worked for:</h3>
+                                <ul>
+                                <li><img src="images/honey.png" alt=""></li>
+                                <li><img src="images/ranbaxy.png" alt=""></li>
+                                <li><img src="images/apac.jpg" alt=""></li>
+                                </ul>
+                                </div>
+                                </div>
+                                </div>
+                                <div class="col-md-6">
+                                <div class="team_container">
+                                <div class="team_photo"><img src="images/abhishek.png" alt=""></div>
+                                <div class="team_description">
+                                <h3>Abhishek Srivastava</h3>
+                                <span> Co-Founder and CEO, MBAtrek Pvt. Ltd.</span> <span>MBA (ISB), MS (Georgia Institute of Technology),</span> <span>BE (Delhi College of Engineering)&nbsp;</span></div>
+                                <div class="contact_Details_team"><label>Mobile: +91-8800466800</label> <label>abhishek.srivastava@mbatrek.com</label></div>
+                                <div class="company_work">
+                                <h3>Companies worked for:</h3>
+                                <ul>
+                                <li><img src="images/apple.jpg" alt=""></li>
+                                <li><img src="images/accenture.png" alt=""></li>
+                                <li><img src="images/schendir.png" alt=""></li>
+                                <li><img src="images/trinity.png" alt=""></li>
+                                </ul>
+                                </div>
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                    </div>
+                </section>
+            </div>
+        </div>
