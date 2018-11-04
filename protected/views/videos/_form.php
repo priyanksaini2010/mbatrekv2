1f9ae382953e40cf807668adc5ea37ae0bef3a15
Priyank Saini <priyanksaini2010@gmail.com>
2018-11-04 17:12:06 +0530

    Issue#127 : Added HTML in POP Up

--- protected/views/videos/_form.php
+++ protected/views/videos/_form.php
@@ -7,23 +7,18 @@
 
 	<?php echo $form->errorSummary($model); ?>
 
-	<?php echo $form->textFieldRow($model,'blog_category_id',array('class'=>'span5')); ?>
-
-	<?php echo $form->textFieldRow($model,'type',array('class'=>'span5')); ?>
+	<?php // echo $form->textFieldRow($model,'type',array('class'=>'span5')); ?>
+        <?php echo $form->dropDownList($model,'type',array(1=>"#InterviewReady",2=>"#InternGo",3=>"CXO's Thought"),array('empty'=>'Select Type', "class"=>"span5")); ?>
 
 	<?php echo $form->textFieldRow($model,'title',array('class'=>'span5','maxlength'=>255)); ?>
 
 	<?php echo $form->textAreaRow($model,'content',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
 
-	<?php echo $form->textFieldRow($model,'author',array('class'=>'span5','maxlength'=>255)); ?>
+	<?php echo $form->textAreaRow($model,'youtube_video_link',array('rows'=>6, 'cols'=>50, 'class'=>'span8')); ?>
 
-	<?php echo $form->textFieldRow($model,'background_image',array('class'=>'span5','maxlength'=>255)); ?>
+	<?php // echo $form->textFieldRow($model,'date_created',array('class'=>'span5')); ?>
 
-	<?php echo $form->textFieldRow($model,'banner_image',array('class'=>'span5','maxlength'=>255)); ?>
-
-	<?php echo $form->textFieldRow($model,'date_created',array('class'=>'span5')); ?>
-
-	<?php echo $form->textFieldRow($model,'date_updated',array('class'=>'span5')); ?>
+	<?php // echo $form->textFieldRow($model,'date_updated',array('class'=>'span5')); ?>
 
 	<div class="form-actions">
 		<?php $this->widget('bootstrap.widgets.TbButton', array(
