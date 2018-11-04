cf71a8c92373be6e7e24f385fde70b14d116b059
Priyank Saini <priyanksaini2010@gmail.com>
2018-11-04 17:12:21 +0530

    Issue#127 : Added HTML in POP Up

--- protected/views/videos/create.php
+++ protected/views/videos/create.php
@@ -1,13 +1,14 @@
 <?php
 $this->breadcrumbs=array(
-	'Videoses'=>array('index'),
-	'Create',
+	'Videos'=>array('admin'),
+	'Manage',
 );
 
 $this->menu=array(
-	array('label'=>'List Videos','url'=>array('index')),
-	array('label'=>'Manage Videos','url'=>array('admin')),
+	array('label'=>'Manage Videos','url'=>array('videos/admin')),
+	array('label'=>'Create Videos','url'=>array('videos/create')),
 );
+
 ?>
 
 <h1>Create Videos</h1>
