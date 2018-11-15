1ec520beb4a07e2aee679015be569825a73ff90a
Priyank Saini <priyanksaini2010@gmail.com>
2018-11-15 22:10:35 +0530

    Issue#127 : Added HTML in POP Up

--- themes/cart/script/validation.js
+++ themes/cart/script/validation.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     //Registeration Form Validation
     $(".cart-remove").click(function(){
-            if(confirm("Are you sure you want to remove this product from your cart?")){
+            if(confirm("Are you sure you wantsdds to remove this product from your cart?")){
                 window.location.href = "https://mbatrek.com/v2/cart/removeCart?p="+$(this).val();
             }
         });
