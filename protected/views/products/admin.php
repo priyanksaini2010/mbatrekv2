1fd140aa36d18c5802a17f14edc0a73e70c5bf8c
Saini, Priyank <Priyank.Saini@fisglobal.com>
2018-11-20 18:53:38 +0530

    WD18462 : Fixed

--- protected/views/products/admin.php
+++ protected/views/products/admin.php
@@ -11,10 +11,46 @@ $this->menu=array(
 ?>
 
 <h1>Manage Products</h1>
-<?php $this->widget('bootstrap.widgets.TbGridView',array(
+<?php
+$str_js = "
+		var fixHelper = function(e, ui) {
+			ui.children().each(function() {
+				$(this).width($(this).width());
+			});
+			return ui;
+		};
+				
+		$('#products-grid table.items tbody').sortable({
+			forcePlaceholderSize: true,
+			forceHelperSize: true,
+			items: 'tr',
+			update : function () {
+                        $('.ui-sortable-handle').removeClass('ui-sortable-handle')
+				serial = $('#products-grid table.items tbody').sortable('serialize', {key: 'items[]', attribute: 'class'});
+				$.ajax({
+					'url': '" . $this->createUrl('products/sort') . "',
+					'type': 'post',
+					'data': serial,
+					'success': function(data){
+					},
+					'error': function(request, status, error){
+						alert('We are unable to set the sort order at this time.  Please try again in a few minutes.');
+					}
+				});
+                                $('#product-key-outcome-grid table.items tr').addClass('ui-sortable-handle');
+			},
+			helper: fixHelper
+		}).disableSelection();
+	";
+Yii::app()->clientScript->registerScript('sortable-project', $str_js);
+
+
+
+$this->widget('bootstrap.widgets.TbGridView',array(
 	'id'=>'products-grid',
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
+      'rowCssClassExpression'=>'"items[]_{$data->id}"',
 	'columns'=>array(
 		
 		'title',
