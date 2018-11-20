56535f9b5286288aa572f56a30feb7ac26824303
Saini, Priyank <Priyank.Saini@fisglobal.com>
2018-11-20 21:28:59 +0530

    WD18462 : Fixed

---
+++ protected/views/site/pages/past_order.php
@@ -0,0 +1,12 @@
+
+<?php $this->setPageTitle('Founders'); ?>
+<div class="bread_crum">
+    <ul class="list-inline list-unstyled">
+	<li><a href="javascript:void(0);"><i class="fa fa-home" aria-hidden="true"></i></a> <i class="fa fa-angle-right" aria-hidden="true"></i></li>
+	<li><a href="<?php echo Yii::app()->createUrl('cart/index'); ?>">Home</a> <i class="fa fa-angle-right" aria-hidden="true"></i></li>
+	<li class="active"><a href="javascript:void(0);">Past Order</a></li>
+    </ul>
+</div>
+<?php $data  = ContentJson::model()->findByAttributes(array("page"=>"past-order"));
+    echo $data->data;
+?>
