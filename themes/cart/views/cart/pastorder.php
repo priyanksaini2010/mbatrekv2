0af76d6c4e83450e9ad1d73b9ef10a165e80d605
priyank <priyanksaini2010@gmail.com>
2019-01-14 12:02:50 +0530

    #414

--- themes/cart/views/cart/pastorder.php
+++ themes/cart/views/cart/pastorder.php
@@ -3,6 +3,13 @@ $baseUrl = Yii::app()->request->baseUrl;
 $orders = CustomerOrder::model()->findAllByAttributes(array("user_id"=>Yii::app()->user->id,"status" => 2));
 $baseUrl = Yii::app()->request->baseUrl;;
 ?>
+
+<div class="bread_crum">
+    <ul class="list-inline list-unstyled">
+        <li><a href="<?php echo Yii::app()->getHomeUrl(); ?>"><i class="fa fa-home" aria-hidden="true"></i></a> <i class="fa fa-angle-right" aria-hidden="true"></i></li>
+        <li class="active"><a href="javascript:void(0);">Past Orders</a></li>
+    </ul>
+</div>
 <div class="page-wrapper">
     <div class="cart_area_wrapper past_orders">
         <div class="container">
