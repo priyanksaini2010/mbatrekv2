9cf1e775f94c73b55410f26ec84baf608bb55e13
Priyank Saini <priyanksaini2010@gmail.com>
2019-01-30 10:35:03 +0530

    Payment Success Page is Updated

--- protected/config/main.php
+++ protected/config/main.php
@@ -213,10 +213,10 @@ return array(
 		'adminEmail'=>'info@mbatrek.com',
 		'noreply'=>'no-reply@mbatrek.com',
         'url'=>'https://mbatrek.com/',
-//        "payu_merchant_id" => "uTUmfIq7",
-//        "payu_salt" => "ZbJSYLBwUJ",
-        "payu_merchant_id" => "gtKFFx",
-        "payu_salt" => "eCwWELxi",
+        "payu_merchant_id" => "uTUmfIq7",
+        "payu_salt" => "ZbJSYLBwUJ",
+//        "payu_merchant_id" => "gtKFFx",
+//        "payu_salt" => "eCwWELxi",
         "payu_url" => "https://secure.payu.in/_payment",
         "payu_test_url" => "https://test.payu.in/_payment"
 //        "payu_test_url" => "https://secure.payu.in/_payment"
