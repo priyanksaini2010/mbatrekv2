f4b1b3325f2fcf5b0194e7045150961be227fea9
Priyank Saini <priyanksaini2010@gmail.com>
2019-01-31 22:14:26 +0530

    Payment Success Page is Updated

--- themes/cart/views/cart/payu.php
+++ themes/cart/views/cart/payu.php
@@ -10,7 +10,7 @@ $paramList["phone"] = $params['mobile'];
 $paramList["hash"] = $params['hash'];
 $paramList["surl"] = $params['surl'];
 $paramList["udf5"] = $params['udf5'];
-$paramList["service_provider"] = $params['payu_paisa'];
+$paramList["service_provider"] = 'payu_paisa';
 ?>
 
 <html>
