585f1ff3ac34c5ee2d1fb1cb1d7730bf0cfcc8e0
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-04 16:23:59 +0530

    Captcha Fixed

--- protected/config/main.php
+++ protected/config/main.php
@@ -214,12 +214,12 @@ return array(
         'adminName'=>'MBAtrek',
 		'noreply'=>'no-reply@mbatrek.com',
         'url'=>'https://mbatrek.com/',
-//        "payu_merchant_id" => "uTUmfIq7",
-//        "payu_salt" => "ZbJSYLBwUJ",
+        "payu_merchant_id" => "uTUmfIq7",
+        "payu_salt" => "ZbJSYLBwUJ",
         "payu_merchant_id" => "rjQUPktU",
-        "payu_salt" => "e5iIg1jwi8",
-//        "payu_test_url" => "https://secure.payu.in/_payment",
-        "payu_test_url" => "https://test.payu.in/_payment"
+//        "payu_salt" => "e5iIg1jwi8",
+        "payu_test_url" => "https://secure.payu.in/_payment",
+//        "payu_test_url" => "https://test.payu.in/_payment"
 //        "payu_test_url" => "https://secure.payu.in/_payment"
 	),
 );
