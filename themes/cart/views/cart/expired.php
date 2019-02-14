6e9bd703b7bff4f331ec7e241e14c86efa532f20
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-15 03:50:22 +0530

    Captcha Fixed

--- themes/cart/views/cart/expired.php
+++ themes/cart/views/cart/expired.php
@@ -1,12 +1,12 @@
 <div class="login_container">
     <div class="container">
         <div class="row">
-            <div class="login_wrapper">
+
 
                 <span> This link has been expired</span>
 
 
-            </div>
+
         </div>
     </div>
 </div>
