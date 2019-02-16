fc9e30f7b2711b6f524795689e6ff01dfa351d69
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-16 15:12:17 +0530

    Captcha Fixed

--- protected/views/site/blogdetails.php
+++ protected/views/site/blogdetails.php
@@ -1,4 +1,11 @@
 <?php $this->setPageTitle($model->title); ?>
+<div class="bread_crum">
+    <ul class="list-inline list-unstyled">
+        <li><a href="<?php echo Yii::app()->getHomeUrl(); ?>"><i class="fa fa-home" aria-hidden="true"></i></a> <i class="fa fa-angle-right" aria-hidden="true"></i></li>
+        <li class="active"><a href="<?php echo Yii::app()->createUrl("blogs");?>">Blogs</a></li>
+        <li class="active"><a href="javascript:void(0);">Blog Details</a></li>
+    </ul>
+</div>
 <div class="blog_new_container_details">
     <div class="blog_header">
     <!--    <div class="container">
