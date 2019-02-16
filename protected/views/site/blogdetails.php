705b4ba4d4238707780f6c22c79115c2890ffa33
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-16 15:13:26 +0530

    Captcha Fixed

--- protected/views/site/blogdetails.php
+++ protected/views/site/blogdetails.php
@@ -2,7 +2,7 @@
 <div class="bread_crum">
     <ul class="list-inline list-unstyled">
         <li><a href="<?php echo Yii::app()->getHomeUrl(); ?>"><i class="fa fa-home" aria-hidden="true"></i></a> <i class="fa fa-angle-right" aria-hidden="true"></i></li>
-        <li class="active"><a href="<?php echo Yii::app()->createUrl("blogs");?>">Blogs</a></li>
+        <li class="active"><a href="<?php echo Yii::app()->createUrl("blogs");?>">Blogs</a> <i class="fa fa-angle-right" aria-hidden="true"></i></li>
         <li class="active"><a href="javascript:void(0);">Blog Details</a></li>
     </ul>
 </div>
