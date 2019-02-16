1060ce7764bd9a462643e2be7ac4620f8519e902
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-16 15:06:00 +0530

    Captcha Fixed

--- themes/cart/views/cart/expired.php
+++ themes/cart/views/cart/expired.php
@@ -1,11 +1,9 @@
 <div class="login_container">
     <div class="container">
         <div class="row">
-
-           <span> This link has been expired</span>
-
-
-
+            <p>This link has been expired.</p>
+            <p>Hey, you have recently changed your password!</p>
+            <p>Try <a href="<?php echo Yii::app()->createUrl("forgot-password");?>">login</a> with the new password, in case you have forgotten your password, kindly click on <a href="<?php echo Yii::app()->createUrl("forgot-password");?>">Forgot Password</a>  to request for the new link.</p>
         </div>
     </div>
 </div>
