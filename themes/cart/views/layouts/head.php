75d9f261f5b5f9e9c5bd481498c309d910eb7979
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-24 15:18:22 +0530

    Captcha Fixed

--- themes/cart/views/layouts/head.php
+++ themes/cart/views/layouts/head.php
@@ -11,11 +11,52 @@
 <link href="<?php echo $baseUrl; ?>/css/jquery.mmenu.css" rel="stylesheet" type="text/css" />
 <!--<link href="--><?php //echo $baseUrl; ?><!--/css/source.css" rel="stylesheet" type="text/css" />-->
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"><!--Google Analytics-->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-90207910-1"></script>
+<!--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-90207910-1"></script>-->
+<!--<script>-->
+<!--    window.dataLayer = window.dataLayer || [];-->
+<!--    function gtag(){dataLayer.push(arguments);}-->
+<!--    gtag('js', new Date());-->
+<!---->
+<!--    gtag('config', 'UA-90207910-1');-->
+<!--</script>-->
+<!-- Global site tag (gtag.js) - Google Ads: 862919331 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-862919331"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-90207910-1');
+    gtag('config', 'AW-862919331');
 </script>
+<script>
+    gtag('event', 'page_view', {
+        'send_to': 'AW-862919331',
+        'dynx_itemid': 'replace with value',
+        'dynx_itemid2': 'replace with value',
+        'dynx_pagetype': 'replace with value',
+        'dynx_totalvalue': 'replace with value',
+        'edu_pagetype': 'replace with value',
+        'edu_pid': 'replace with value',
+        'edu_plocid': 'replace with value',
+        'edu_totalvalue': 'replace with value',
+        'job_id': 'replace with value',
+        'job_locid': 'replace with value',
+        'job_pagetype': 'replace with value',
+        'job_totalvalue': 'replace with value',
+        'user_id': 'replace with value'
+    });
+</script>
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2220470411551001');
+    fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2220470411551001&ev=PageView&noscript=1" /></noscript>
+
