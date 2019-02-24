f94888ad0239c814d906c604766f65898534356d
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-24 14:22:46 +0530

    Captcha Fixed

--- themes/cart/views/layouts/foot.php
+++ themes/cart/views/layouts/foot.php
@@ -1,7 +1,7 @@
 <?php $baseUrl = (Yii::app()->theme ? $baseUrl : Yii::app()->request->baseUrl . "/themes/cart"); ?>
 		<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
 		<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js'></script>
-		<script src="http://d39ivqqvyriko0.cloudfront.net/source.js"></script>
+		<script src="https://d39ivqqvyriko0.cloudfront.net/source.js"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script type="text/javascript">
 	$(function() {
