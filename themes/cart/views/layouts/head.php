874ad3fe446277b3235a148d753e0b99d6f83d7b
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-24 13:39:37 +0530

    Minified CSS and Serve From S3

--- themes/cart/views/layouts/head.php
+++ themes/cart/views/layouts/head.php
@@ -3,12 +3,13 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link href="<?php echo $baseUrl; ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo $baseUrl; ?>/css/style.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo $baseUrl; ?>/css/new-style.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo $baseUrl; ?>/css/animate.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo $baseUrl; ?>/css/font-awesome.css" rel="stylesheet" type="text/css" />
-<link href="<?php echo $baseUrl; ?>/css/jquery.mmenu.css" rel="stylesheet" type="text/css" />
+<!--<link href="--><?php //echo $baseUrl; ?><!--/css/bootstrap.min.css" rel="stylesheet" type="text/css" />-->
+<!--<link href="--><?php //echo $baseUrl; ?><!--/css/style.css" rel="stylesheet" type="text/css" />-->
+<!--<link href="--><?php //echo $baseUrl; ?><!--/css/new-style.css" rel="stylesheet" type="text/css" />-->
+<!--<link href="--><?php //echo $baseUrl; ?><!--/css/animate.css" rel="stylesheet" type="text/css" />-->
+<!--<link href="--><?php //echo $baseUrl; ?><!--/css/font-awesome.css" rel="stylesheet" type="text/css" />-->
+<!--<link href="--><?php //echo $baseUrl; ?><!--/css/jquery.mmenu.css" rel="stylesheet" type="text/css" />-->
+<link href="https://s3.amazonaws.com/mbatrek/source.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"><!--Google Analytics-->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-90207910-1"></script>
 <script>
