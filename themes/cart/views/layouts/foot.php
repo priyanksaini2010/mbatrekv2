f0a30b90d53fdcb7bd0804afea0d53811df83bf5
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-24 14:51:23 +0530

    Captcha Fixed

--- themes/cart/views/layouts/foot.php
+++ themes/cart/views/layouts/foot.php
@@ -2,7 +2,7 @@
 		<script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
 		<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js'></script>
 		<script src="https://d39ivqqvyriko0.cloudfront.net/source.js"></script>
-		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<!--		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>-->
 <script type="text/javascript">
 	$(function() {
 		$('nav#menu').mmenu();
