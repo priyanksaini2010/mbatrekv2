8e1208d29016e8a90e9f3c131d00433a85101a5a
Priyank Saini <priyanksaini2010@gmail.com>
2019-02-24 14:05:32 +0530

    Captcha Fixed

--- themes/cart/views/layouts/head.php
+++ themes/cart/views/layouts/head.php
@@ -9,7 +9,7 @@
 <!--<link href="--><?php //echo $baseUrl; ?><!--/css/animate.css" rel="stylesheet" type="text/css" />-->
 <!--<link href="--><?php //echo $baseUrl; ?><!--/css/font-awesome.css" rel="stylesheet" type="text/css" />-->
 <!--<link href="--><?php //echo $baseUrl; ?><!--/css/jquery.mmenu.css" rel="stylesheet" type="text/css" />-->
-<link href="https://s3.amazonaws.com/mbatrek/source.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $baseUrl; ?>/css/source.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"><!--Google Analytics-->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-90207910-1"></script>
 <script>
