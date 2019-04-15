6e83418d6be7210dc1857d1aa4cbbdaa729f45da
Priyank Saini <priyanksaini2010@gmail.com>
2019-04-15 21:38:28 +0530

    0000567: Competitions URL and Tab Name

---
+++ themes/cart/views/cart/review.php
@@ -0,0 +1,36 @@
+<div class="page-wrapper">
+    <div class="review_page">
+        <div class="container">
+            <div class="review_block">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="reviw_content">
+                        <h1>Did you like the service provided by MBAtrek for this product ?</h1>
+                        <h2>Tap on the stars to rate the product </h2>
+                        <h3>Feedback and Review<br>
+                            (Welcome your opinions)*</h3>
+
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="reviw_content">
+                        <ul class="like_ul">
+                            <li><a href="javascript:void(0);"><img src="images/likeimg.png"/></a></li>
+                            <li><a href="javascript:void(0);"><img src="images/likeimg.png"/></a></li>
+                        </ul>
+                        <ul class="star_ul">
+                            <li><a href="javascript:void(0);"><img src="images/rating.png"/></a></li>
+                            <li><a href="javascript:void(0);"><img src="images/rating.png"/></a></li>
+                            <li><a href="javascript:void(0);"><img src="images/rating.png"/></a></li>
+                            <li><a href="javascript:void(0);"><img src="images/rating.png"/></a></li>
+                            <li><a href="javascript:void(0);"><img src="images/rating.png"/></a></li>
+                        </ul>
+                        <textarea></textarea>
+                    </div>
+                </div>
+                <div class="col-md-6 text-right">
+                    <input type="submit" value="Submit" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
